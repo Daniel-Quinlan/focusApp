7f3cb4835f7baef1f195929f94873978f9b8da8a
Daniel Quinlan <daquinlan@iinet.net.au>
2025-02-24 17:59:49 +1100

    fixed gitignore

--- server/connect.cjs
+++ server/connect.cjs
@@ -1,20 +1,22 @@
-const {MongoClient} = require("mongodb")
-require("dotenv").config({path: "./config.env"})
+const { MongoClient } = require("mongodb");
+require("dotenv").config({ path: "./config.env" });
 
-async function main () {
-    const Db = process.env.ATLAS_URI
-    const client = new MongoClient(Db)
-    try {
-        await client.connect()
-        console.log("Connected to the database")
-        const collection = await client.db("FocusApp").collections()
-        collection.forEach((collection) => (console.log(collection.s.namespace.collection)))
-    } catch (error) {
-        console.log("Error connecting to the database", error)
-    } finally {
-        await client.close()
-        console.log("Connection closed")
-    }
+async function main() {
+  const Db = process.env.ATLAS_URI;
+  const client = new MongoClient(Db);
+  try {
+    await client.connect();
+    console.log("Connected to the database");
+    const collection = await client.db("FocusApp").collections();
+    collection.forEach((collection) =>
+      console.log(collection.s.namespace.collection)
+    );
+  } catch (error) {
+    console.log("Error connecting to the database", error);
+  } finally {
+    await client.close();
+    console.log("Connection closed");
+  }
 }
 
-main()
+main();
